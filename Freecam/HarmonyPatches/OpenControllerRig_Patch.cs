e1467667a60919981796d94715e3c0160b6d727e
Millzy <millzyyt08@gmail.com>
2024-02-13 23:47:31 +0000

    this needs to be false for the game to unpause fsr

---
+++ Freecam/HarmonyPatches/OpenControllerRig_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SLZ.Rig;
+
+namespace Freecam.HarmonyPatches;
+
+[HarmonyPatch(typeof(OpenControllerRig))]
+[HarmonyPatch(nameof(OpenControllerRig.OnStart))]
+internal static class OpenControllerRig_Patch
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void Postfix(OpenControllerRig __instance)
+    {
+        __instance._isControllerRigPaused = true;
+    }
+}
