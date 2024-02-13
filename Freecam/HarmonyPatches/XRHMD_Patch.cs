db566d6dc303d3d597295e1e8bbc4e80c78c3b12
Millzy <millzyyt08@gmail.com>
2024-02-13 23:47:16 +0000

    change to prefix to hopefully speed up execution

--- Freecam/HarmonyPatches/XRHMD_Patch.cs
+++ Freecam/HarmonyPatches/XRHMD_Patch.cs
@@ -1,16 +1,21 @@
 ﻿using HarmonyLib;
+using MelonLoader;
 using SLZ.Marrow.Input;
 
 namespace Freecam.HarmonyPatches;
 
 // ReSharper disable once InconsistentNaming
 [HarmonyPatch(typeof(XRHMD))]
-[HarmonyPatch(nameof(XRHMD.IsUserPresent))]
+[HarmonyPatch(nameof(XRHMD.IsUserPresent), MethodType.Getter)]
 internal static class XRHMD_Patch
 {
-    [HarmonyPostfix]
-    private static void Postfix(bool __result)
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once RedundantAssignment
+    // ReSharper disable once UnusedMember.Local
+    [HarmonyPrefix]
+    private static bool Prefix(ref bool __result)
     {
         __result = true;
+        return false;
     }
 }
