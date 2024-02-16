3636720f66ebe657800f04663d33d3ae0dafeae1
Millzy <millzyyt08@gmail.com>
2024-02-16 18:59:25 +0000

    toggle camera with key

--- Freecam/FreecamHostManager.cs
+++ Freecam/FreecamHostManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Freecam.Configuration;
 using MelonLoader;
 using UnityEngine;
 
@@ -7,10 +8,13 @@ namespace Freecam;
 [RegisterTypeInIl2Cpp]
 internal sealed class FreecamHostManager(IntPtr ptr) : MonoBehaviour(ptr)
 {
+    private Config _config = null!;
+    private GameObject _freecamObject = null!;
+    
     public static void CreateFreecam(Transform playerHead)
     {
         var freecamHost = new GameObject("FreecamHost");
-        freecamHost.AddComponent<FreecamHostManager>();
+        _ = freecamHost.AddComponent<FreecamHostManager>();
 
         Transform hostTransform = freecamHost.transform;
         hostTransform.position = playerHead.position;
@@ -18,9 +22,36 @@ internal sealed class FreecamHostManager(IntPtr ptr) : MonoBehaviour(ptr)
 
         var freecamObject = new GameObject("Freecam");
         freecamObject.transform.SetParent(hostTransform, false);
+        freecamObject.transform.localPosition = new Vector3(0f, 0f, 0f);
 
         var camera = freecamObject.AddComponent<Camera>();
         camera.cameraType = CameraType.Game;
-        // TODO: Add camera control behaviour
+
+        _ = freecamObject.AddComponent<FreecamController>();
+    }
+
+    private void Awake()
+    {
+        _config = Config.Instance;
+    }
+
+    private void Start()
+    {
+        _freecamObject = GetComponentInChildren<FreecamController>().gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleFreecam();
+        }
+    }
+
+    private void ToggleFreecam()
+    {
+        bool newFreecamActivity = !_config.FreecamEnabled;
+        _freecamObject.SetActive(newFreecamActivity);
+        _config.FreecamEnabled = newFreecamActivity;
     }
 }
