f73e645ea7abb44e087f9997036cc3f1c5e95cd7
Millzy <millzyyt08@gmail.com>
2024-02-16 19:00:27 +0000

    static config

--- Freecam/HarmonyPatches/XRHMD_get_IsUserPresent.cs
+++ Freecam/HarmonyPatches/XRHMD_get_IsUserPresent.cs
@@ -9,13 +9,15 @@ namespace Freecam.HarmonyPatches;
 [HarmonyPatch(nameof(XRHMD.IsUserPresent), MethodType.Getter)]
 internal static class XRHMD_get_IsUserPresent
 {
+    private static readonly Config s_config = Config.Instance;
+    
     // ReSharper disable once InconsistentNaming
     // ReSharper disable once RedundantAssignment
     // ReSharper disable once UnusedMember.Local
     [HarmonyPrefix]
     private static bool Prefix(ref bool __result)
     {
-        if (!Config.Instance.FreecamEnabled)
+        if (!s_config.FreecamEnabled)
             return false;
         
         // To stop the game from pausing because no headset is on, we simply tell it "nuh uh the headset is still awake"
