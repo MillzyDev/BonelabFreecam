9993581832e58dbdd93b22c4c909035844739175
Millzy <millzyyt08@gmail.com>
2024-02-16 23:58:09 +0000

    no xr loader required

--- Freecam/Mod.cs
+++ Freecam/Mod.cs
@@ -1,13 +1,33 @@
 ﻿using Freecam.Configuration;
+using Il2CppSystem.Collections.Generic;
 using MelonLoader;
+using Unity.XR.MockHMD;
+using UnityEngine;
+using UnityEngine.XR.Management;
 
 namespace Freecam
 {
     public sealed class Mod : MelonMod
     {
+        private static void SwapXRLoader()
+        {
+            List<XRLoader> loaders = XRGeneralSettings.Instance.Manager.loaders;
+            
+            var mockHmdLoader = ScriptableObject.CreateInstance<MockHMDLoader>();
+            mockHmdLoader.name = "Freecam Override";
+            
+            loaders.Clear();
+            loaders.Add(mockHmdLoader);
+        }
+        
         public override void OnInitializeMelon()
         {
             PatchManager.Instance.InstallFreecamPatches();
+
+            if (Config.Instance.NoHmd)
+            {
+                SwapXRLoader();
+            }
         }
 
         public override void OnApplicationQuit()
