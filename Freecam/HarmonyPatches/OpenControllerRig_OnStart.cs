944f10572fc94a88a6545349567b6741c696517e
Millzy <millzyyt08@gmail.com>
2024-02-16 10:37:41 +0000

    rename and add commnets

--- Freecam/HarmonyPatches/OpenControllerRig_OnStart.cs
+++ Freecam/HarmonyPatches/OpenControllerRig_OnStart.cs
@@ -5,13 +5,14 @@ namespace Freecam.HarmonyPatches;
 
 [HarmonyPatch(typeof(OpenControllerRig))]
 [HarmonyPatch(nameof(OpenControllerRig.OnStart))]
-internal static class OpenControllerRig_Patch
+internal static class OpenControllerRig_OnStart
 {
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
     // ReSharper disable once UnusedMember.Local
     private static void Postfix(OpenControllerRig __instance)
     {
+        // The game won't unpause unless its already paused (slz tryna make my life difficult smh)
         __instance._isControllerRigPaused = true;
     }
 }
--- Freecam/HarmonyPatches/XRHMD_get_IsUserPresent.cs
+++ Freecam/HarmonyPatches/XRHMD_get_IsUserPresent.cs
@@ -7,7 +7,7 @@ namespace Freecam.HarmonyPatches;
 // ReSharper disable once InconsistentNaming
 [HarmonyPatch(typeof(XRHMD))]
 [HarmonyPatch(nameof(XRHMD.IsUserPresent), MethodType.Getter)]
-internal static class XRHMD_Patch
+internal static class XRHMD_get_IsUserPresent
 {
     // ReSharper disable once InconsistentNaming
     // ReSharper disable once RedundantAssignment
@@ -15,6 +15,7 @@ internal static class XRHMD_Patch
     [HarmonyPrefix]
     private static bool Prefix(ref bool __result)
     {
+        // To stop the game from pausing because no headset is on, we simply tell it "nuh uh the headset is still awake"
         __result = true;
         return false;
     }
