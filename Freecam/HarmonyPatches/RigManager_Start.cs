d91a48120ccbe4663b25df015043357b32c50506
Millzy <millzyyt08@gmail.com>
2024-02-16 10:44:23 +0000

    create freecam on rigmanager start

---
+++ Freecam/HarmonyPatches/RigManager_Start.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using SLZ.Bonelab;
+using SLZ.Rig;
+using SLZ.SaveData;
+
+namespace Freecam.HarmonyPatches;
+
+[HarmonyPatch(typeof(RigManager))]
+[HarmonyPatch(nameof(RigManager.Start))]
+internal static class RigManager_Start
+{
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void Postfix(RigManager __instance)
+    {
+        // Switch spectator camera to passthrough so the spectator camera can work.
+        Control_Player controlPlayer = __instance.uiRig.controlPlayer;
+        DataManager.Settings._spectatorSettings._spectatorCameraMode = SpectatorCameraMode.Passthrough;
+        controlPlayer.UpdateSpectator();
+        
+        FreecamHostManager.CreateFreecam(__instance.ControllerRig.m_head);
+    }
+}
