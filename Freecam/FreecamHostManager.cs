baf11226e21b74938c682a9a231bb80327a80545
Millzy <millzyyt08@gmail.com>
2024-02-16 10:38:01 +0000

    create freecam object

---
+++ Freecam/FreecamHostManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Freecam;
+
+internal sealed class FreecamHostManager(IntPtr ptr) : MonoBehaviour(ptr)
+{
+    public static void CreateFreecam(Transform playerHead)
+    {
+        var freecamHost = new GameObject("FreecamHost");
+        freecamHost.AddComponent<FreecamHostManager>();
+
+        Transform hostTransform = freecamHost.transform;
+        hostTransform.position = playerHead.position;
+        hostTransform.rotation = playerHead.rotation;
+
+        var freecamObject = new GameObject("Freecam");
+        freecamObject.transform.SetParent(hostTransform, false);
+
+        var camera = freecamObject.AddComponent<Camera>();
+        camera.cameraType = CameraType.Game;
+        // TODO: Add camera control behaviour
+    }
+}
